8dc3408760b548207e22108d5cecbfbc7aa2d22d
CangShYao <1937636639@qq.com>
2022-01-19 17:55:10 +0800

    219问题提交

--- src/gocode/main/main.go
+++ src/gocode/main/main.go
@@ -12,4 +12,8 @@ func main() {
 	second := "jkypmsxd"
 	learngo.Range_go()
 	fmt.Println(leetcode.FindOcurrences(text, first, second))
+	// 测试int最大值
+	// const INT_MAX = int(^uint(0) >> 1)
+	// const INT_MIN = ^INT_MAX
+	// fmt.Println(INT_MIN)
 }
